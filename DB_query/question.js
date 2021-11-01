a5a44126e030c1fd261e287a38e5b47381594305
NEWSIX <nitigon.phad@gmail.com>
2021-11-01 21:25:25 +0700

    db

---
+++ DB_query/question.js
@@ -0,0 +1,34 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+var mydatabase = "thesisDB";
+var mycollection = "questions";
+
+
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db(mydatabase);
+    var query = { answer:/.*m*/};
+    dbo.collection(mycollection).find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result[0].question);
+      db.close();
+    });
+  });
+
+
+/*
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db(mydatabase);
+  var cursor = db.collection(mycollection).find();
+        cursor.each(function(err, item) {
+            console.log(item);
+    if(item == null) {
+        db.close();
+        return;
+    }
+    });
+});
+*/
