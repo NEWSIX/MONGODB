6c794613620582f610cf898aa751a9f8e9a1beed
NEWSIX <nitigon.phad@gmail.com>
2021-11-01 18:10:27 +0700

    /db

--- routes/db.js
+++ routes/db.js
@@ -7,27 +7,52 @@ var mydatabase = "thesisDB";
 var mycollection = "movie";
 
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
 
+/* get db page */
+router.post('/',function(req,res){
+    var fistname = req.body.fname;
+    var lasttname = req.body.lname;
+    var adrrs = req.body.addr;
+    console.log(fistname);
+    console.log(lasttname);
+    console.log(adrrs);
+
+
+    var myobj = {name:fistname+" "+lasttname,
+    address:adrrs};
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-        var dbo = db.db(mydatabase);
-        dbo.collection(mycollection).findOne({}, function(err, result) {
-          if (err) throw err;
-          console.log("\n\n########\n\n");
-          console.log(result);
-          console.log("\n\n########\n\n");
-          db.close();
-          res.render('db', { title: '你爱我吗？' 
-          ,db_name: result.name
-          ,db_age: result.age
-          });
+        var dbo = db.db(mydatabase);  
+        dbo.collection(mycollection).insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("\n\n1 document inserted\n\n");
+            db.close();
         });
-      });
+    });
+    //res.location('/');
+    res.redirect('/db');
+    
+});
 
 
-  
+/* query db page. */
+router.get('/', function(req, res, next) {
+
+  MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db(mydatabase);
+      dbo.collection(mycollection).findOne({}, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+        res.render('db', 
+            { title: '你爱我吗？' 
+            ,db_name: result.name
+            ,db_age: result.age
+            });
+        });
+     });
+
 });
 
 module.exports = router;
